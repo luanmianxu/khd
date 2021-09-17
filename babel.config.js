828c5b918954e253c7ab9b5c084f10a009d4dc59
huangyanhui <2262612341@qq.com>
2021-09-17 12:20:00 +0800

    测lmx

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  "presets": ['@vue/cli-plugin-babel/preset',["es2015", { "modules": false }]],
-  "plugins": [
-    [
-      "component",
-      {
-        "libraryName": "element-ui",
-        "styleLibraryName": "theme-chalk"
-      }
-    ]
-  ]
+  "presets": ['@vue/cli-plugin-babel/preset',],
+ 
 }
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/style/communal.less'
-import { Pagination } from 'element-ui';
+import element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 
-Vue.use(Pagination)
+Vue.use(elem)
 new Vue({
   router,
   store,
